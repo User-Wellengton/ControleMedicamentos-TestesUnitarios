04e2e2599dae7a6c6f564420a985e1d7f0fe9523
User-Wellengton <wellengtonjm@gmail.com>
2022-06-15 17:03:12 -0300

    att

---
+++ ControleMedicamentos.Dominio.Tests/ModuloFornecedor/ValidadorFornecedorTeste.cs
@@ -0,0 +1,29 @@
+﻿
+
+using ControleMedicamentos.Dominio.ModuloFornecedor;
+using FluentValidation.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ControleMedicamentos.Dominio.Tests.ModuloFornecedor
+{
+    [TestClass]
+    public class ValidadorFornecedorTeste
+    {
+        [TestMethod]
+        public void NaoDeve_SerNulo_Nome()
+        {
+            Fornecedor fornecedor = new(null, "99665577", "fornecedor@gmail.com", "Lages", "SC");
+
+            ValidadorFornecedor validacao = new();
+
+            //action
+            ValidationResult resultado = validacao.Validate(fornecedor);
+
+            //assert
+            Assert.AreEqual("'Nome' não pode ser nulo", resultado.Errors[0].ErrorMessage);
+        }
+
+        
+        
+    }
---
+++ ControleMedicamentos.Dominio.Tests/ModuloPaciente/ValidadorPacienteTeste.cs
@@ -0,0 +1,79 @@
+﻿using ControleMedicamentos.Dominio.ModuloPaciente;
+using FluentValidation.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace ControleMedicamentos.Dominio.Tests.ModuloPaciente
+{
+    [TestClass]
+
+    public  class ValidadorPacienteTeste
+    {
+        [TestMethod]
+        public void Nome_nao_Pode_Ser_Nulo()
+        {
+            //arrange
+            Paciente paciente = new(null, "321654987");
+
+            ValidadorPaciente validacao = new();
+
+            //action
+            ValidationResult resultado = validacao.Validate(paciente);
+
+            //assert
+            Assert.AreEqual("Campo 'Nome' Não pode ser nulo", resultado.Errors[0].ErrorMessage);
+           
+        }
+
+        [TestMethod]
+        public void Nome_nao_Pode_Ser_Vazio()
+        {
+            //arrange
+            Paciente paciente = new("", "321654987");
+
+            ValidadorPaciente validacao = new();
+
+            //action
+            ValidationResult resultado = validacao.Validate(paciente);
+
+            //assert
+            Assert.AreEqual("Campo 'Nome' Não pode ser vazio", resultado.Errors[0].ErrorMessage);
+
+        }
+
+        [TestMethod]
+        public void Cartao_nao_Pode_Ser_Nulo()
+        {
+            //arrange
+            Paciente paciente = new("Paciente1", null);
+
+            ValidadorPaciente validacao = new();
+
+            //action
+            ValidationResult resultado = validacao.Validate(paciente);
+
+            //assert
+            Assert.AreEqual("Campo 'CartaoSUS' Não pode ser nulo", resultado.Errors[0].ErrorMessage);
+            
+        }
+
+        [TestMethod]
+        public void Cartao_nao_Pode_Ser_Vazio()
+        {
+            //arrange
+            Paciente paciente = new("Paciente1", "");
+
+            ValidadorPaciente validacao = new();
+
+            //action
+            ValidationResult resultado = validacao.Validate(paciente);
+
+            //assert
+            Assert.AreEqual("Campo 'CartaoSUS' Não pode ser vazio", resultado.Errors[0].ErrorMessage);
+
+        }
+
+
+    }
+    
+}
