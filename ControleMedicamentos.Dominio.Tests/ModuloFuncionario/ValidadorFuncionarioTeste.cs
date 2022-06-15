f30223f76f253bc22a50d6ef07adb4bfd19def2c
User-Wellengton <wellengtonjm@gmail.com>
2022-06-15 17:42:04 -0300

    att

---
+++ ControleMedicamentos.Dominio.Tests/ModuloFuncionario/ValidadorFuncionarioTeste.cs
@@ -0,0 +1,95 @@
+﻿using ControleMedicamentos.Dominio.ModuloFuncionario;
+using FluentValidation.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControleMedicamentos.Dominio.Tests.ModuloFuncionario
+{
+    [TestClass]
+    public class ValidadorFuncionarioTeste
+    {
+        [TestMethod]
+        public void Nome_nao_Pode_Ser_Nulo()
+        {
+            Funcionario funcionario = new(null, "Login1", "Senha1");
+
+            ValidadorFuncionario validacao = new();
+
+            //action
+            ValidationResult resultado = validacao.Validate(funcionario);
+
+            //assert
+            Assert.AreEqual("Campo 'Nome' não pode ser nulo", resultado.Errors[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Nome_nao_Pode_Ser_Vazio()
+        {
+            Funcionario funcionario = new("", "Login1", "Senha1");
+
+            ValidadorFuncionario validacao = new();
+
+            //action
+            ValidationResult resultado = validacao.Validate(funcionario);
+
+            //assert
+            Assert.AreEqual("Campo 'Nome' não pode ser vazio", resultado.Errors[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Login_nao_Pode_Ser_Nulo()
+        {
+            Funcionario funcionario = new("Funcionario1",null, "Senha1");
+
+            ValidadorFuncionario validacao = new();
+
+            //action
+            ValidationResult resultado = validacao.Validate(funcionario);
+
+            //assert
+            Assert.AreEqual("Campo 'Login' não pode ser nulo", resultado.Errors[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Login_nao_Pode_Ser_Vazio()
+        {
+            Funcionario funcionario = new("Funcionario1", "", "Senha1");
+
+            ValidadorFuncionario validacao = new();
+
+            //action
+            ValidationResult resultado = validacao.Validate(funcionario);
+
+            //assert
+            Assert.AreEqual("Campo 'Login' não pode ser vazio", resultado.Errors[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Senha_nao_Pode_Ser_Nulo()
+        {
+            Funcionario funcionario = new("Funcionario1", "Login1", null);
+
+            ValidadorFuncionario validacao = new();
+
+            //action
+            ValidationResult resultado = validacao.Validate(funcionario);
+
+            //assert
+            Assert.AreEqual("Campo 'Senha' não pode ser nulo", resultado.Errors[0].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Senha_nao_Pode_Ser_Vazio()
+        {
+            Funcionario funcionario = new("Funcionario1", "Login1", "");
+
+            ValidadorFuncionario validacao = new();
+
+            //action
+            ValidationResult resultado = validacao.Validate(funcionario);
+
+            //assert
+            Assert.AreEqual("Campo 'Senha' não pode ser vazio", resultado.Errors[0].ErrorMessage);
+        }
+
+    }
+}
